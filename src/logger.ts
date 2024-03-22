2d7af28ec948252aa2f54b79aa6fe45a41fe3f3f
Carlos Freitas <carlos.freitas@pravaler.com.br>
2024-03-22 14:25:16 -0300

    :sparkles: Adiciona funções de logger e atualiza dependências

---
+++ src/logger.ts
@@ -0,0 +1,41 @@
+import { LoggerService, Injectable, Logger } from '@nestjs/common';
+
+export interface LogContext {
+  requestId: string;
+  channel: string;
+  message: string;
+  description?: string;
+  context: {
+    accountability: string;
+    category: string;
+    method: string;
+    exception?: Error;
+    dataApi?: any;
+  };
+}
+
+@Injectable()
+export class CustomLogger implements LoggerService {
+  private logger = new Logger();
+
+  constructor(private serviceName: string) {}
+
+  log(message: string) {
+    this.logger.log(message);
+  }
+
+  error(message: string, trace: string) {
+    this.logger.error(message, trace);
+  }
+
+  warn(context: LogContext) {
+    const logMessage = {
+      ...context,
+      context: {
+        ...context.context,
+        category: this.serviceName,
+      },
+    };
+    this.logger.warn(logMessage);
+  }
+}
