8a201d7b68c86823c81b662b4a7ce89bda478caa
Carlos Freitas <carlos.freitas@pravaler.com.br>
2024-03-24 17:15:34 -0300

    :pencil2: Updated interface

--- src/logger.ts
+++ src/logger.ts
@@ -1,19 +1,5 @@
 import { LoggerService, Injectable, Logger } from '@nestjs/common';
-import { ErrorLogContext } from './interfaces/LoggerProps';
-
-export interface LogContext {
-  requestId: string;
-  channel: string;
-  message: string;
-  description?: string;
-  context: {
-    accountability: string;
-    category: string;
-    method: string;
-    exception?: Error;
-    dataApi?: any;
-  };
-}
+import { ErrorLogContext, LogContext } from './interfaces/LoggerProps';
 
 @Injectable()
 export class CustomLogger implements LoggerService {
